21b2cb68d0d55e46bf3006258f37f9f3cef3ed90
Kenneth Cole <70181196+coleken@users.noreply.github.com>
2021-08-09 20:45:52 -0500

    feat: Add instruction toast message

--- app/src/main/java/dev/kelocen/loadstatus/main/MainActivity.kt
+++ app/src/main/java/dev/kelocen/loadstatus/main/MainActivity.kt
@@ -3,6 +3,7 @@ package dev.kelocen.loadstatus.main
 import android.app.DownloadManager
 import android.content.IntentFilter
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import dev.kelocen.loadstatus.R
 import dev.kelocen.loadstatus.receiver.DownloadReceiver
@@ -10,6 +11,9 @@ import dev.kelocen.loadstatus.util.getDownload
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
+/**
+ * The main activity for the Load Status application.
+ */
 class MainActivity : AppCompatActivity() {
 
     private var receiver = DownloadReceiver()
@@ -19,8 +23,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
-        custom_button.setOnClickListener {
-            getDownload(this)
+        custom_button_download.setOnClickListener {
+            if (radio_group_item_list.checkedRadioButtonId == -1) {
+                displayInstructionToast()
+            } else {
+                getDownload(this)
+            }
         }
+    }
+
+    /**
+     * Displays a toast message that instructs the user to select an item to download.
+     */
+    private fun displayInstructionToast() {
+        Toast.makeText(this, getString(R.string.instruct_to_download), Toast.LENGTH_SHORT).show()
     }
 }

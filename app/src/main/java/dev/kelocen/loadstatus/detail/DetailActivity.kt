6e88fef41f75def35cae855287a042763502821e
Kenneth Cole <70181196+coleken@users.noreply.github.com>
2021-08-13 23:44:17 -0500

    feat: Add notification action

--- app/src/main/java/dev/kelocen/loadstatus/detail/DetailActivity.kt
+++ app/src/main/java/dev/kelocen/loadstatus/detail/DetailActivity.kt
@@ -1,18 +1,36 @@
 package dev.kelocen.loadstatus.detail
 
+import android.app.NotificationManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import dev.kelocen.loadstatus.R
-import kotlinx.android.synthetic.main.activity_detail.*
+import androidx.core.content.ContextCompat
+import dev.kelocen.loadstatus.databinding.ActivityDetailBinding
+import dev.kelocen.loadstatus.databinding.ContentDetailBinding
+import dev.kelocen.loadstatus.util.cancelNotifications
 
 /**
  * An [AppCompatActivity] subclass for the detail screen.
  */
 class DetailActivity : AppCompatActivity() {
 
+    private lateinit var detail: ActivityDetailBinding
+    private lateinit var content: ContentDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
-        setSupportActionBar(toolbar)
+        detail = ActivityDetailBinding.inflate(layoutInflater)
+        content = detail.contentDetail
+        setContentView(detail.root)
+        setSupportActionBar(detail.toolbar)
+        cancelNotifications()
+    }
+
+    /**
+     * Cancels notifications when the notification action button is used to open the detail screen.
+     */
+    private fun cancelNotifications() {
+        val notificationManager =
+            ContextCompat.getSystemService(this, NotificationManager::class.java)
+        notificationManager?.cancelNotifications()
     }
 }
--- app/src/main/java/dev/kelocen/loadstatus/util/NotificationUtils.kt
+++ app/src/main/java/dev/kelocen/loadstatus/util/NotificationUtils.kt
@@ -1,5 +1,6 @@
 package dev.kelocen.loadstatus.util
 
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
@@ -9,34 +10,37 @@ import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import dev.kelocen.loadstatus.R
-import dev.kelocen.loadstatus.main.MainActivity
+import dev.kelocen.loadstatus.detail.DetailActivity
 import dev.kelocen.loadstatus.util.Constants.NOTIFICATION_ID
-
-private lateinit var pendingIntent: PendingIntent
 
 /**
  * Sends a notification with the given message and [Context].
  */
 fun NotificationManager.sendNotification(message: String, applicationContext: Context) {
-    val downloadIntent = Intent(applicationContext, MainActivity::class.java)
-    pendingIntent = PendingIntent.getActivity(applicationContext, NOTIFICATION_ID, downloadIntent,
-            PendingIntent.FLAG_IMMUTABLE)
-    notify(NOTIFICATION_ID, getNotification(message, applicationContext).build())
+    val detailIntent = Intent(applicationContext, DetailActivity::class.java)
+    val pendingIntent = PendingIntent.getActivity(applicationContext, NOTIFICATION_ID, detailIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
+    val notificationAction = NotificationCompat.Action(null,
+            applicationContext.getString(R.string.notification_button), pendingIntent)
+    notify(NOTIFICATION_ID,
+            getNotification(message, applicationContext, notificationAction, pendingIntent))
 }
 
 /**
- * Returns a [NotificationCompat.Builder] with the given message and context.
+ * Returns a [Notification] with the given message, context, action, and intent.
  */
-private fun getNotification(message: String,
-                            applicationContext: Context): NotificationCompat.Builder {
+private fun getNotification(message: String, applicationContext: Context,
+                            notificationAction: NotificationCompat.Action,
+                            pendingIntent: PendingIntent): Notification {
     return NotificationCompat.Builder(applicationContext,
             applicationContext.getString(R.string.notification_channel_id))
-        .setSmallIcon(R.drawable.ic_assistant_black_24dp)
+        .setSmallIcon(R.drawable.ic_download_assistant)
         .setContentTitle(applicationContext.getString(R.string.notification_title))
         .setContentText(message)
         .setContentIntent(pendingIntent)
         .setAutoCancel(true)
-        .setPriority(NotificationCompat.PRIORITY_HIGH)
+        .setPriority(NotificationCompat.DEFAULT_ALL)
+        .addAction(notificationAction).build()
 }
 
 /**
@@ -46,7 +50,7 @@ private fun getNotification(message: String,
 fun createChannel(context: Context, channelId: String, channelName: String) {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
         val notificationChannel = NotificationChannel(channelId, channelName,
-                NotificationManager.IMPORTANCE_HIGH).apply {
+                NotificationManager.IMPORTANCE_DEFAULT).apply {
             setShowBadge(true)
         }
         val notificationManager = context.getSystemService(NotificationManager::class.java)
